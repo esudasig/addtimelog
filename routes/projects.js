cfd813ec9f8b1b6ae6f11e8eb51008f29849e6f0
esudasig <82942515+esudasig@users.noreply.github.com>
2021-08-19 18:33:39 +0530

    Delete routes directory

--- routes/projects.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-import { addTimelog } from '../controllers/projects.js'; 
-
-const router = express.Router();
-
-router.post('/', addTimelog);
-export default router;
